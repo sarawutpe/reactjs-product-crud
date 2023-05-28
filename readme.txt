d19840871450cf52bb2a04f8dbb9fbb0d269011e
Sarawut Pe <mr.sarawutpe@gmail.com>
2023-05-28 16:44:44 +0700

    install react router

--- readme.txt
+++ readme.txt
@@ -1,3 +1,13 @@
+How to add package?
+$ yarn add [PACKAGE NAME]
+
+How to delete package?
+$ yarn remove [PACKAGE NAME]
+
+install axios Manage HTTP Client GET POST PUT DEL
+https://axios-http.com/docs/intro
+$ yarn add axios
 
 install react router
+https://reactrouter.com/en/main
 $ yarn add react-router-dom react-dom
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <p>404 Not Found!</p>,
+    children: [
+      {
+        path: '',
+        element: <App />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
